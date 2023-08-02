251b9babc3a2ddd6126568a1c22c405edffc6733
rubel <rubel.cse532@gmail.com>
2023-08-02 12:32:56 +0600

    code refactor

--- src/App.js
+++ src/App.js
@@ -4,22 +4,13 @@ import searchIcon from "./search.svg";
 import {useEffect, useState} from "react";
 import MovieCard from "./MovieCard";
 const API_URL = 'http://www.omdbapi.com?apikey=c032e2d7';
-const movie1 = {
-    "Title": "Italian Spiderman",
-    "Year": "2007",
-    "imdbID": "tt2705436",
-    "Type": "movie",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BZWQxMjcwNjItZjI0ZC00ZTc4LWIwMzItM2Q0YTZhNzI3NzdlXkEyXkFqcGdeQXVyMTA0MTM5NjI2._V1_SX300.jpg"
-}
 function App() {  
     const [movies, setMovies] = useState([]);
     const [searchTerm, setSearchTerm] = useState("")
     const searchMovies = async (title) => {
         const response = await fetch(`${API_URL}&s=${title}`);
         const data = await response.json();
-        setMovies(data.Search);
-        console.log(movies);
-        
+        setMovies(data.Search);        
     }
     useEffect(() => {
         searchMovies(searchTerm);
@@ -51,9 +42,9 @@ function App() {
                             }
                          </div>
                     ) : (
-                        <div className="empty">
-                            <h2>No movies found</h2>
-                        </div>
+                    <div className="empty">
+                        <h2>No movies found</h2>
+                    </div>
                 )
             }
           </div>
